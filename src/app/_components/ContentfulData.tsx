f35c57a82df8fe6593b3277056b47ac4bae6c425
MooKorea <69235304+MooKorea@users.noreply.github.com>
2024-06-24 17:11:13 -0500

    fix type error

--- src/app/_components/ContentfulData.tsx
+++ src/app/_components/ContentfulData.tsx
@@ -1,3 +1,9 @@
+//Contentful GraphQL documentation:
+//https://www.contentful.com/developers/docs/references/graphql/#/introduction
+
+//GraphiQL IDE for inspecting the schema:
+//https://graphql.contentful.com/content/v1/spaces/{SPACE_ID}/explore?access_token={ACCESS_TOKEN}
+
 export default async function ContentfulData(query: string, tags: string) {
   const data = await fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID!}/`, {
     method: "POST",
--- src/app/api/revalidation/route.ts
+++ src/app/api/revalidation/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { revalidateTag } from "next/cache";
 
-export async function POST(request: { headers: HeadersInit | undefined; }) {
+export async function POST(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
   const secret = requestHeaders.get("x-vercel-reval-key");
 
